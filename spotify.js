842cc9c53081c7fc6fd2ab6781f31b0c1183fcf4
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2017-04-17 21:56:35 -0400

    :space_invader: started spotify

--- spotify.js
+++ spotify.js
@@ -1,18 +1,24 @@
+const argv = require('minimist')(process.argv.slice(2))
+const chalk = require('chalk')
 const spotify = require('spotify')
 
 const queryTypes = ['artist', 'album'];
 
-spotify.search({ type: queryTypes.join(','), query: 'new order', limit: 1 }, (err, data) => {
+if (!argv._.length) {
+  console.error(chalk.red(`Please specify a query!`))
+  process.exit(1)
+}
+
+spotify.search({ type: queryTypes.join(','), query: argv._[0], limit: 1 }, (err, data) => {
   if ( err ) {
     console.log('Error occurred: ' + err)
     return
   }
  
-  console.log('found stuff!')
-
+  console.log('')
   queryTypes.forEach((item) => {
-    console.log('---------------------------------------------------')
-    console.log(item, data[item + 's'].items[0])
+    console.log(chalk.red(item), data[item + 's'].items[0])
   })
+  console.log('')
 
 })
