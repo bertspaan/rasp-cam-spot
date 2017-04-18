e0fda2abe8af5309a5b09c35c95421f086aa6f61
Bert Spaan <bert.spaan@gmail.com>
2017-04-17 20:57:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# rasp-cam-spot
+
+1. Take picture!
+2. Do Google Image Search or image-to-keyword thing
+3. Search Spotify!
+4. Play! https://developer.spotify.com/web-api/web-api-connect-endpoint-reference/
---
+++ index.js
@@ -0,0 +1,15 @@
+const vision = require('@google-cloud/vision')
+
+// var visionClient = vision({
+//   projectId: 'grape-spaceship-123',
+//   keyFilename: '/path/to/keyfile.json'
+// });
+
+// // Read the text from an image.
+// visionClient.detectText('./image.jpg', function(err, text) {
+//   // text = [
+//   //   'This was text found in the image',
+//   //   'This was more text found in the image'
+//   // ]
+// });
+
